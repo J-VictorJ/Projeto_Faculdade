0692376d7895ef9d715057f25f4a9ae390f66161
João Victor <jvictor@192.168.1.8>
2025-06-09 19:41:13 -0300

    Adicionando o Lázaro

--- frontend/src/components/pages/Pet/PetDetails.js
+++ frontend/src/components/pages/Pet/PetDetails.js
@@ -1,11 +1,11 @@
-import api from "../../../utils/api";
+/*import api from "../../../utils/api";
 
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 
 import styles from "./PetDetails.module.css";
 
-/* hooks */
+/* hooks 
 import useFlashMessage from "../../../hooks/useFlashMessage";
 
 function PetDetails() {
@@ -76,6 +76,66 @@ function PetDetails() {
         </section>
       )}
     </>
+  );
+}
+
+export default PetDetails;
+*/
+
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+import styles from "./PetDetails.module.css";
+
+function PetDetails() {
+  const [pet] = useState({
+    _id: "1",
+    name: "Lázaro",
+    weight: 50,
+    age: 6,
+    available: false,
+    images: ["lazaro.jpeg", "lazaro2.jpeg"],
+    ownerContact: {
+      nome: "ADM",
+      email: "joao@email.com",
+      telefone: "(61) 99999-8888",
+    },
+  });
+
+  return (
+    <section className={styles.pet_details_container}>
+      <div className={styles.petdetails_header}>
+        <h1>Conhecendo o Kchorrinho: {pet.name}</h1>
+        <p>Se tiver interesse, entre em contato com o responsável.</p>
+      </div>
+      <div className={styles.pet_images}>
+        {pet.images.map((image, index) => (
+          <img
+            key={index}
+            src={`/images/pets/${image}`}
+            alt={pet.name}
+          />
+        ))}
+      </div>
+      <p>
+        <span className="bold">Peso:</span> {pet.weight}kg
+      </p>
+      <p>
+        <span className="bold">Idade:</span> {pet.age} anos
+      </p>
+      <p>
+        <span className="bold">Responsável:</span> {pet.ownerContact.nome}
+      </p>
+      <p>
+        <span className="bold">Email:</span> {pet.ownerContact.email}
+      </p>
+      <p>
+        <span className="bold">Telefone:</span> {pet.ownerContact.telefone}
+      </p>
+      <p className={styles.note}>
+        * Essa é uma simulação acadêmica para fins de projeto. O cão apresentado aqui é fictício.
+      </p>
+    </section>
   );
 }
 
